a2458951ceb3952be2fe7534a3437ebff6522188
gessicaminski <gessicaminski@gmail.com>
2022-05-25 16:57:16 -0300

    Create 0_pacotes.r

---
+++ 2_script/0_pacotes.r
@@ -0,0 +1,44 @@
+##pacotes utilizados 
+
+#instalando pacotes
+install.packages("raster")
+install.packages("rgdal")
+install.packages("ncdf4")
+install.packages("sp")
+install.packages("dplyr")
+install.packages("countrycode")
+install.packages("stringr")
+install.packages("tidyverse")
+install.packages("htmlwidgets")
+install.packages("textclean")
+install.packages("rgeos")
+install.packages("gtools")
+install.packages("enc")
+install.packages("maptools")
+install.packages("data.table") 
+install.packages("stringr")   
+
+# inicializando as funcoes de cortar -esta na pasta funçção crop
+source("C:/Users/gessi/rasterCrop.R")
+
+
+#abrindo pacotes
+library(ncdf4)
+library(sp)
+library(raster)
+library(rgdal)
+library(dplyr)
+library(countrycode)
+library(stringr)
+library(tidyverse)
+#library(htmlwidgets)
+library(textclean)
+library(rgeos)
+#library(gtools)
+library(enc)
+library(maptools)
+library(sf)
+library(rmapshaper)
+library(RColorBrewer)     
+library(stringr)                                                      
+library(data.table)       
